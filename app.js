e43e9bd689381731d6b28e755e5958efcb624a60
AngerOverApathy <violasian@gmail.com>
2023-05-07 19:10:31 -0700

    winnining msg made

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ let boxes = Array.from(document.getElementsByClassName('box'))
 const X_SYMBOL = 'X'
 const O_SYMBOL = 'O'
 
+const winningCombo = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
 let spaces = Array(9).fill(null)
 let currentPlayer = X_SYMBOL
 
@@ -19,7 +30,23 @@ function playGame(el){
         el.target.innerText = currentPlayer
 
         currentPlayer = currentPlayer == X_SYMBOL ? O_SYMBOL : X_SYMBOL
+
+        if(playerHasWon() !== false){
+            player = `${currentPlayer} has won!`
+        }
+
     }
+}
+
+function playerHasWon() { 
+    for (const condition of winningCombo){
+        let [a, b, c] = condition
+
+        if (spaces[a] && (spaces[a] == spaces[b]) && (spaces[a] == spaces[c])){ //if array spaces equal each other, the combo is a winner
+            return [a,b,c]
+        } 
+    }
+    return false
 }
 
 function restartGame(){
@@ -27,9 +54,11 @@ function restartGame(){
 
    boxes.forEach( box => {
     box.innerText = ''
+
+    currentPlayer = X_SYMBOL
    })
 
-   currentPlayer = X_SYMBOL
+   player = 'Tic Tac Toe'
 }
 
 startGame()
