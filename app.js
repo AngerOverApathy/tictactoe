ace9ba249dc055d2c20fa93143bd5a6348672770
AngerOverApathy <violasian@gmail.com>
2023-05-07 18:51:56 -0700

    player functionality added

---
+++ app.js
@@ -0,0 +1,24 @@
+let button = document.getElementById('restartBtn')
+let player = document.getElementById('player')
+let boxes = Array.from(document.getElementsByClassName('box'))
+
+const X_SYMBOL = 'X'
+const O_SYMBOL = 'O'
+
+let spaces = Array(9).fill(null)
+let currentPlayer = X_SYMBOL
+
+const startGame = () => boxes.forEach(box => box.addEventListener('click', playGame))     
+
+function playGame(el){
+    const id = el.target.id
+
+    if(!spaces[id]){
+        spaces[id] = currentPlayer
+        el.target.innerText = currentPlayer
+
+        currentPlayer = currentPlayer == X_SYMBOL ? O_SYMBOL : X_SYMBOL
+    }
+}
+
+startGame()
